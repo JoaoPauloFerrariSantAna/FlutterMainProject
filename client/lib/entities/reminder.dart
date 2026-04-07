854f63aa00758ae388bad1c2da6032d5dff3b388
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-07 19:46:55 -0300

    feat: added entities

---
+++ client/lib/entities/reminder.dart
@@ -0,0 +1,7 @@
+class Reminder
+{
+  final String title;
+  final String description;
+
+  const Reminder({ required this.title, required this.description});
+}
---
+++ client/lib/entities/user.dart
@@ -0,0 +1,14 @@
+class User
+{
+  final int uid;
+  final String username;
+  final String email;
+  final String password;
+
+  const User({
+     required this.uid,
+     required this.username,
+     required this.email,
+     required this.password
+    });
+}
