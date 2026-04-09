7cabb3a86acd3f914a3bf40d5b39d529a8690400
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-09 20:45:19 -0300

    ref: separate the app class from main

---
+++ client/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:client/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Homepage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,54 +1,6 @@
-import 'package:client/constants/defaults/sized_box_default_height.dart';
-import 'package:client/screens/login_screen.dart';
-import 'package:client/screens/register_screen.dart';
-import 'package:client/widgets/change_screen_button.dart';
+import 'package:client/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ProtoAlpha")
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const ChangeScreenButton(name: "Register", screenToChange: RegisterScreen()),
-            const SizedBox(height: sizedBoxDefaultHeight),
-            const ChangeScreenButton(name: "Login", screenToChange: LoginScreen())
-          ]
-        )
-      )
-    );
-  }
+  runApp(const App());
 }
